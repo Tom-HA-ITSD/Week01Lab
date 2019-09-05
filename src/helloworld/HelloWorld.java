370e6a37a49b401d575c62348af83a1c793fd8e2
768661 <768661@ELF8IT221608.ACDM.DS.SAIT.CA>
2019-09-05 17:43:15 -0600

    Project Created Created the HelloWorld Project

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ * This Program greets the world
+ * @author 768661
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
